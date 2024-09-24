c265e77c2277bf0f77aa7f68711906e23d00339b
Godwin Jemegah <godwinjdayo@gmail.com>
2024-09-24 18:04:24 +0100

    changes

---
+++ app/page-old2.tsx
@@ -0,0 +1,13 @@
+import Leaderboard from '../components/Leaderboard';
+import UpdateUserStat from '../components/UpdateUserStat';
+import toast, { Toaster } from 'react-hot-toast';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen p-4 bg-white">
+    <div><Toaster/></div>
+      <h1 className="text-3xl font-bold text-center text-gray-900 mb-8">Leaderboard App</h1>
+      <Leaderboard />
+    </div>
+  );
+}
